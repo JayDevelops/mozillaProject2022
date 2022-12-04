432eca94a5b0c88ce6b6e5b7d5c32ab9006c041e
ObeseWallets <69029356+ObeseWallets@users.noreply.github.com>
2022-12-04 00:37:34 -0800

    new

--- studybox/popup/pomodoro.js
+++ studybox/popup/pomodoro.js
@@ -1,40 +1,40 @@
-// variables
-
-let workTitle = document.getElementById('work');
-let breakTitle = document.getElementById('break');
+let workTittle = document.getElementById('work');
+let breakTittle = document.getElementById('break');
 
 let workTime = 25;
 let breakTime = 5;
 
-let seconds = "00";
+let seconds = "00"
 
 // display
-
 window.onload = () => {
-    document.getElementById('minutes').textContent = String(workTime);
-    document.getElementById('seconds').textContent = seconds;
+    document.getElementById('minutes').innerHTML = workTime;
+    document.getElementById('seconds').innerHTML = seconds;
 
-    workTitle.classList.add('active');
+    workTittle.classList.add('active');
 }
 
-// starting timer
-
+// start timer
 function start() {
-    //change the time
-    seconds=59;
+    // change button
+    document.getElementById('start').style.display = "none";
+    document.getElementById('reset').style.display = "block";
+
+    // change the time
+    seconds = 59;
 
     let workMinutes = workTime - 1;
     let breakMinutes = breakTime - 1;
 
-    let breakCount = 0;
+    breakCount = 0;
 
     // countdown
-
     let timerFunction = () => {
+        //change the display
+        document.getElementById('minutes').innerHTML = workMinutes;
+        document.getElementById('seconds').innerHTML = seconds;
 
-        document.getElementById('minutes').textContent = workMinutes;
-        document.getElementById('seconds').textContent = seconds;
-
+        // start
         seconds = seconds - 1;
 
         if(seconds === 0) {
@@ -45,22 +45,23 @@ function start() {
                     workMinutes = breakMinutes;
                     breakCount++
 
-                    // changing the painel
-                    workTitle.classList.remove('active');
-                    breakTitle.classList.add('active');
+                    // change the painel
+                    workTittle.classList.remove('active');
+                    breakTittle.classList.add('active');
                 }else {
                     // continue work
                     workMinutes = workTime;
                     breakCount++
 
-                    // changing the painel
-                    breakTitle.classList.remove('active');
-                    workTitle.classList.add('active');
+                    // change the painel
+                    breakTittle.classList.remove('active');
+                    workTittle.classList.add('active');
                 }
             }
             seconds = 59;
+        }
     }
-    setInterval(timerFunction, 1000);
-}
 
+    // start countdown
+    setInterval(timerFunction, 1000); // 1000 = 1s
 }
