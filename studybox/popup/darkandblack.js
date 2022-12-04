2727a5b8ba9142c44aa21263e87bcc0b2be8da05
ObeseWallets <69029356+ObeseWallets@users.noreply.github.com>
2022-12-03 23:43:51 -0800

    new

--- studybox/popup/darkandblack.js
+++ studybox/popup/darkandblack.js
@@ -6,13 +6,13 @@ toggle.addEventListener('click',function()
     this.classList.toggle('bi-moon');
     if(this.classList.toggle('bi-brightness-high-fill'))
     {
-        body.style.background = '#f0e7db';
-        body.style.color = '#202023';
+        body.style.background = '#f0efeb';
+        body.style.color = '#264653';
         body.style.transition ='2s';
     }
     else{
-        body.style.background = '#202023';
-        body.style.color = '#f0e7db';
+        body.style.background = '#264653';
+        body.style.color = '#f0efeb';
         body.style.transition ='2s';
     
     }
