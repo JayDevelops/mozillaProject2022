f6dcf2a2659c57a448412b4e124ade524f8bfd65
Jay.develops <jaymations1234@gmail.com>
2022-12-04 23:14:39 -0800

    Replaced body selector with individual selectors

--- studybox/popup/darkandblack.js
+++ studybox/popup/darkandblack.js
@@ -1,24 +1,52 @@
 const toggle = document.getElementById('toggleDark');
 const body = document.querySelector('body');
-const rightContent = document.getElementsByClassName('rightContent');
+const musicPlayer = document.getElementById('music-container');
+const clock = document.getElementById('clockContainer');
+const playList = document.getElementById('playlist-container');
+const quoteContainer = document.getElementById('quoteContainer');
+const actionButtons = document.getElementsByClassName('action-btn');
 
-toggle.addEventListener('click',function()
-{
+toggle.addEventListener('click',function() {
     this.classList.toggle('bi-moon');
-    if(this.classList.toggle('bi-brightness-high-fill'))
-    {
+    if(this.classList.toggle('bi-brightness-high-fill')) {
+        // Toggle light mode below
         body.style.background = '#f0efeb';
-        body.style.color = '#264653';
-        body.style.transition ='2s';
+        musicPlayer.style.backgroundColor = '#ffffff';
+        clock.style.color = '#000000';
+        playList.style.color = '#000000';
+        playList.style.backgroundColor = '#ffffff';
+        playList.style.boxShadow = '0 5px 20px rgba(252, 169, 169, 0.6)';
+        quoteContainer.style.color = '#000000';
+        Array.from(actionButtons).forEach(button => button.style.backgroundColor = '#ffffff');
 
-        rightContent.style.color = '#000000';
-    }
-    else{
+        //  Transitions below
+        body.style.transition ='2s';
+        clock.style.transition = '2s';
+        musicPlayer.style.transition = '2s';
+        playList.style.transition = '2s';
+        quoteContainer.style.transition = '2s';
+
+        Array.from(actionButtons).forEach(button => button.style.transition = '2s');
+    } else{
+        // Toggle dark mode below
         body.style.background = '#264653';
-        body.style.color = '#f0efeb';
-        body.style.transition ='2s';
+        clock.style.color = '#ffffff';
+        musicPlayer.style.backgroundColor = '#000000';
+        playList.style.backgroundColor = '#000000';
+        playList.style.color = '#ffffff';
+        playList.style.boxShadow = '0 5px 20px rgba(255, 255, 255, 0.6)';
+        quoteContainer.style.color = '#ffffff';
 
-        rightContent.style.color = '#ffffff';
+        Array.from(actionButtons).forEach(button => button.style.backgroundColor = '#000000');
+
+        //  Transitions below
+        body.style.transition ='2s';
+        clock.style.transition = '2s';
+        musicPlayer.style.transition = '2s';
+        playList.style.transition = '2s';
+        quoteContainer.style.transition = '2s';
+
+        Array.from(actionButtons).forEach(button => button.style.transition = '2s');
     }
 
 })
