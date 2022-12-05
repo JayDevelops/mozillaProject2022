bb2c24c6c260b53972cc060bc02979bfebd7c3f6
Jay.develops <jaymations1234@gmail.com>
2022-12-04 20:43:42 -0800

    fixed dark mode

--- studybox/popup/darkandblack.js
+++ studybox/popup/darkandblack.js
@@ -1,5 +1,6 @@
 const toggle = document.getElementById('toggleDark');
 const body = document.querySelector('body');
+const rightContent = document.getElementsByClassName('rightContent');
 
 toggle.addEventListener('click',function()
 {
@@ -9,14 +10,15 @@ toggle.addEventListener('click',function()
         body.style.background = '#f0efeb';
         body.style.color = '#264653';
         body.style.transition ='2s';
+
+        rightContent.style.color = '#000000';
     }
     else{
         body.style.background = '#264653';
         body.style.color = '#f0efeb';
         body.style.transition ='2s';
-    
+
+        rightContent.style.color = '#ffffff';
     }
-
-
 
 })
